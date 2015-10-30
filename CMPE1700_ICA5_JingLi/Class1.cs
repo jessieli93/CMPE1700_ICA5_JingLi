5e2a05aa43d80f695e3adafe5f2fea3a504a6bf4
jessieli93 <jli83@VDI-CNT2015-129.nait.ca>
2015-10-30 10:30:53 -0600

    ICA
    Cards

---
+++ CMPE1700_ICA5_JingLi/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPE1700_ICA5_JingLi
+{
+    struct card
+    {
+        public enum suits
+        {
+            club,
+            spade,
+            heart,
+            diamond
+        }
+        public enum face
+        {
+            Ace,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King
+        }
+
+        public string Suits;
+        public string faces;
+        public override string ToString()
+        {
+            return faces + " of " + Suits;
+        }
+    }
+}
---
+++ CMPE1700_ICA5_JingLi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPE1700_ICA5_JingLi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            card[] cards = new card[52];
+            int index = 0;
+            foreach (card.suits s in Enum.GetValues((typeof(card.suits))))
+            {
+                foreach (card.face f in Enum.GetValues((typeof(card.face))))
+                {
+                    cards[index].Suits = s.ToString();
+                    cards[index++].faces = f.ToString();
+
+                }
+            }
+            foreach (card i in cards)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    
+    }
+}
